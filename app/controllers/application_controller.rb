e727d35427e7ade9a24ce6ebd5ece6da0a4eada8
Daniel Perez <daniel@claudetech.com>
2014-12-14 05:37:15 +0900

    Add rescue from when not authorized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :user_signed_in?
 
+  if Rails.env != 'development'
+    rescue_from GoodsDbApi::AuthenticationError, with: :return_401
+  end
+
   def current_user
     @user ||= User.find_by(token: request.headers['X-Token'])
   end
@@ -13,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     fail GoodsDbApi::AuthenticationError, 'not logged in' unless user_signed_in?
+  end
+
+  def return_401
+    render json: { error: 'not authorized' }, status: :unauthorized
   end
 end
